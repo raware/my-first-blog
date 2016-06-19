f00f46581284e471c5f17572144b4a5a9d79eff7
raware <borowski@aol.pl>
2016-06-19 10:46:11 +0200

    '0.1'

---
+++ test.py
@@ -0,0 +1,6 @@
+pies = {'ilosc_nog':4, 'dlugosc_siersci':'krotka', 'rasa':'owczarek', 'imie':'Rex', 'ulubiona_karma':'Pedigre', 'wiek':2}
+
+def imie_psa(_pies):
+	print _pies['imie']
+
+imie_psa(pies)
