4f7c185d8b7beab136340e00e5148ffd171898b1
raware <borowski@aol.pl>
2016-06-19 17:37:09 +0200

    kommenty + logowanie

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+LOGIN_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATICFILES_DIRS = (
